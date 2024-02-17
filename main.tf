992a8229905da0c8682740c9a66825c64cea07d5
ySodias <63486008+ySodias@users.noreply.github.com>
2024-02-17 13:19:10 -0300

    teste terraform org

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "restaurante-34"
+    organization = "restaurant-34"
 
     workspaces {
       name = "tech-challenge"
