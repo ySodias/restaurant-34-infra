c9045bc0dfedfa8198b26925a507622508bcfc8d
ySodias <63486008+ySodias@users.noreply.github.com>
2024-02-17 23:04:06 -0300

    config aws_eks_cluster cluster policy

--- main.tf
+++ main.tf
@@ -35,6 +35,10 @@ resource "aws_eks_cluster" "cluster" {
   vpc_config {
     subnet_ids = ["subnet-074ef0d265d707d83", "subnet-01420c8586c83826f"]
   }
+
+  depends_on = [
+    arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
+  ]
 }
 
 output "endpoint" {
