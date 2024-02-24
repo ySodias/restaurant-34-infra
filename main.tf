14dbbf1c4fccd0f9aef589c9b0f837d4f051d03d
ySodias <63486008+ySodias@users.noreply.github.com>
2024-02-24 11:21:18 -0300

    formatando terraform
    formantando terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = var.region
+  region = var.region
 }
 #****** VPC Start ******#
 
@@ -74,7 +74,7 @@ resource "aws_security_group" "k8s_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
     from_port   = 2379
     to_port     = 2380
@@ -112,15 +112,15 @@ resource "aws_security_group" "k8s_sg" {
 #****** VPC END ******#
 
 resource "random_string" "s3name" {
-  length = 9
+  length  = 9
   special = false
-  upper = false
-  lower = true
+  upper   = false
+  lower   = true
 }
 
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket = aws_s3_bucket.s3buckit.id
-  acl    = "private"
+  bucket     = aws_s3_bucket.s3buckit.id
+  acl        = "private"
   depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
@@ -132,74 +132,74 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
 }
 
 resource "aws_s3_bucket" "s3buckit" {
-  bucket = "k8s-${random_string.s3name.result}"
+  bucket        = "k8s-${random_string.s3name.result}"
   force_destroy = true
- depends_on = [
+  depends_on = [
     random_string.s3name
   ]
 }
 
 
 resource "aws_instance" "ec2_instance_msr" {
-    ami = var.ami_id
-    subnet_id = aws_subnet.some_public_subnet.id
-    instance_type = var.instance_type
-    key_name = var.ami_key_pair_name
-    associate_public_ip_address = true
-    security_groups = [ aws_security_group.k8s_sg.id ]
-    root_block_device {
-    volume_type = "gp2"
-    volume_size = "8"
+  ami                         = var.ami_id
+  subnet_id                   = aws_subnet.some_public_subnet.id
+  instance_type               = var.instance_type
+  key_name                    = var.ami_key_pair_name
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.k8s_sg.id]
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = "8"
     delete_on_termination = true
-    }
-    tags = {
-        Name = "k8s_msr_1"
-    }
-    user_data_base64 = base64encode("${templatefile("scripts/install_k8s_msr.sh", {
+  }
+  tags = {
+    Name = "k8s_msr_1"
+  }
+  user_data_base64 = base64encode("${templatefile("scripts/install_k8s_msr.sh", {
 
-    access_key = var.access_key
-    private_key = var.secret_key
-    region = var.region
+    access_key    = var.access_key
+    private_key   = var.secret_key
+    region        = var.region
     s3buckit_name = "k8s-${random_string.s3name.result}"
-    })}")
+  })}")
 
-    depends_on = [
+  depends_on = [
     aws_s3_bucket.s3buckit,
     random_string.s3name
   ]
 
-    
-} 
+
+}
 
 resource "aws_instance" "ec2_instance_wrk" {
-    ami = var.ami_id
-    count = var.number_of_worker
-    subnet_id = aws_subnet.some_public_subnet.id
-    instance_type = var.instance_type
-    key_name = var.ami_key_pair_name
-    associate_public_ip_address = true
-    security_groups = [ aws_security_group.k8s_sg.id ]
-    root_block_device {
-    volume_type = "gp2"
-    volume_size = "8"
+  ami                         = var.ami_id
+  count                       = var.number_of_worker
+  subnet_id                   = aws_subnet.some_public_subnet.id
+  instance_type               = var.instance_type
+  key_name                    = var.ami_key_pair_name
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.k8s_sg.id]
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = "8"
     delete_on_termination = true
-    }
-    tags = {
-        Name = "k8s_wrk_${count.index + 1}"
-    }
-    user_data_base64 = base64encode("${templatefile("scripts/install_k8s_wrk.sh", {
+  }
+  tags = {
+    Name = "k8s_wrk_${count.index + 1}"
+  }
+  user_data_base64 = base64encode("${templatefile("scripts/install_k8s_wrk.sh", {
 
-    access_key = var.access_key
-    private_key = var.secret_key
-    region = var.region
+    access_key    = var.access_key
+    private_key   = var.secret_key
+    region        = var.region
     s3buckit_name = "k8s-${random_string.s3name.result}"
     worker_number = "${count.index + 1}"
 
-    })}")
-  
-    depends_on = [
-      aws_s3_bucket.s3buckit,
-      random_string.s3name,
-      aws_instance.ec2_instance_msr
+  })}")
+
+  depends_on = [
+    aws_s3_bucket.s3buckit,
+    random_string.s3name,
+    aws_instance.ec2_instance_msr
   ]
 } 
