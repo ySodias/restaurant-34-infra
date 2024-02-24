0a14a30e96d79f469bf0e6ccf8bc1c90f76cea0c
ySodias <63486008+ySodias@users.noreply.github.com>
2024-02-24 11:52:51 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
-  token    = var.session_token
+  token      = var.session_token
 }
 #****** VPC Start ******#
 
