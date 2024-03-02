4bbc31e3ac6c04b74adf4470f1bd3031ec9e1364
ySodias <63486008+ySodias@users.noreply.github.com>
2024-03-02 11:00:45 -0300

    change source branch

--- .github/workflows/validate_diff.sh
+++ .github/workflows/validate_diff.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SOURCE=${1:-master}
+SOURCE=${1:-main}
 TARGET=${2:-HEAD}
 FILTER=${3:-.*}
 exec git diff $SOURCE $TARGET -- $(cat <(git ls-tree --name-only -r $SOURCE) <(git ls-tree --name-only -r $TARGET) | sort | uniq | grep "$FILTER")
